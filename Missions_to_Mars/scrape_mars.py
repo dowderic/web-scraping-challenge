f2a98791c4d6295f02b28c92fbdbfc57051bc8f7
Eric Dowd <dowderic@hotmail.com>
2021-02-10 22:12:52 -0500

    .

---
+++ Missions_to_Mars/scrape_mars.py
@@ -0,0 +1,6 @@
+#importing dependencies to scrape Mars Nasa New website
+from bs4 import BeautifulSoup as bs
+import requests
+from webdriver_manager.chrome import ChromeDriverManager
+from splinter import Browser
+
